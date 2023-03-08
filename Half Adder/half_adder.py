526a2e8ebef14e788c7d3df9a64f67e4975760f1
SoardRaspi <69724218+SoardRaspi@users.noreply.github.com>
2023-03-08 09:44:49 +0530

    Update half_adder.py

--- Half Adder/half_adder.py
+++ Half Adder/half_adder.py
@@ -2,20 +2,18 @@ from qiskit import *
 from pylatexenc import *
 from qiskit.tools.visualization import plot_histogram
 
-qc = QuantumCircuit(3, 2)
-qc.x(0)
-qc.x(1)
+qc = QuantumCircuit(4, 2)
 
-qc.ccx(0, 1, 2)
-qc.cx(0, 1)
+qc.x([0, 1])
+qc.cx(0, 2)
+qc.cx(1, 2)
+qc.ccx(0, 1, 3)
 
 qc.barrier()
-qc.measure(2, 1)
-qc.measure(1, 0)
+qc.measure([2, 3], [0, 1])
 
 %matplotlib inline
-
-qc.draw(output = "mpl")
+qc.draw(output="mpl")
 
 simulator = Aer.get_backend("qasm_simulator")
 result = execute(qc, backend = simulator, shots = 1).result()
