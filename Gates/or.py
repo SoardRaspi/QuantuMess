cedb0f1453d41440cf03f3fe823f3f35b77fdc96
SoardRaspi <69724218+SoardRaspi@users.noreply.github.com>
2023-03-08 11:41:59 +0530

    Update or.py

--- Gates/or.py
+++ Gates/or.py
@@ -1,1 +1,25 @@
+from qiskit import QuantumCircuit, QuantumRegister
+from qiskit.quantum_info.operators import Operator
 
+qc = QuantumCircuit(3, 1)
+
+qc.x(0)
+qc.x(1)
+qc.x(2)
+
+cor = Operator([
+    [1, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 1, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 1, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 1],
+    [0, 1, 0, 0, 0, 0, 0, 0],
+    [0, 0, 1, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 1, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 1, 0]
+])
+qc.unitary(cor, [0, 1, 2], label='cor')
+
+qc.measure(2, 0)
+
+%matplotlib inline
+qc.draw(output="mpl")
