90dbde83670bebdd32c43141312599b85fdb21ea
SoardRaspi <69724218+SoardRaspi@users.noreply.github.com>
2023-03-08 12:09:30 +0530

    Update or.py

--- Gates/or.py
+++ Gates/or.py
@@ -1,7 +1,11 @@
-from qiskit import QuantumCircuit, QuantumRegister
+from qiskit import QuantumCircuit, QuantumRegister, ClassicalRegister
 from qiskit.quantum_info.operators import Operator
 
-qc = QuantumCircuit(3, 1)
+in0 = QuantumRegister(1, 'input 1')
+in1 = QuantumRegister(1, 'input 2')
+c = QuantumRegister(1, 'control')
+meas = ClassicalRegister(1, 'meas')
+qc = QuantumCircuit(in0, in1, c, meas)
 
 qc.x(0)
 qc.x(1)
@@ -9,15 +13,14 @@ qc.x(2)
 
 cor = Operator([
     [1, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 1, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 1, 0, 0],
+    [0, 0, 0, 0, 0, 0, 1, 0],
     [0, 0, 0, 0, 0, 0, 0, 1],
     [0, 1, 0, 0, 0, 0, 0, 0],
     [0, 0, 1, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 1, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 1, 0]
+    [0, 0, 0, 1, 0, 0, 0, 0],
+    [0, 0, 0, 0, 1, 0, 0, 0]
 ])
-qc.unitary(cor, [0, 1, 2], label='cor')
 
 qc.measure(2, 0)
 
